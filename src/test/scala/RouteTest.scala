d822b23bf857900d8514b626e52601b3f85db971
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-07-25 14:54:38 +0200

    additional tests

--- src/test/scala/RouteTest.scala
+++ src/test/scala/RouteTest.scala
@@ -4,37 +4,41 @@ import de.guderlei.spray.database.DatabaseConfiguration
 import de.guderlei.spray.domain.TodoItem
 import org.specs2.mutable.Specification
 import spray.testkit.Specs2RouteTest
-import spray.routing.HttpService
 import de.guderlei.spray.api._
-import spray.httpx.SprayJsonSupport._
 import spray.http.StatusCodes._
+import java.util.Date
 
 class RouteTest  extends Specification with Specs2RouteTest  with TodoWebService with DatabaseConfiguration{
     def actorRefFactory = system // connect the DSL to the test ActorSystem
+   // start the backend actor, the database is started using the DatabaseConfiguration mixin
     actorRefFactory.actorOf(Props[TodoItemActor], "todo-service")
+   var id: Long = 0
 
   "The service" should {
-
-      "return list of todoitems" in {
+      "return empty list of todoitems" in {
         Get("/items") ~> myRoute ~> check {
           status === OK
           val content = entityAs[List[TodoItem]]
-          print(content)
           content must beEmpty
         }
       }
-      /*
-      "return a 'PONG!' response for GET requests to /ping" in {
-        Get("/ping") ~> myRoute ~> check {
-          entityAs[String] === "PONG!"
+
+      "return a todoitem when trying to create a valid new one" in {
+        Post("/items", new TodoItem(-1, new Date(), "test")) ~> myRoute ~> check {
+          val item = entityAs[TodoItem]
+          id = item.id
+          status === OK
+          item.text === "test"
         }
       }
 
-      "leave GET requests to other paths unhandled" in {
-        Get("/kermit") ~> myRoute ~> check {
-          handled must beFalse
+     "the previously created item by its direct url" in {
+        Get("/items/" + id.toString) ~> myRoute ~> check {
+          status === OK
+          val item = entityAs[Option[TodoItem]]
+          item.get.text === "test"
         }
-      }     */
+      }
     }
 
 }
